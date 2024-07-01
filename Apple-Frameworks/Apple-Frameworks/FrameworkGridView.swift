fa4a0202cb8be28a4d39987fb6243de46dd7a34f
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2024-07-01 12:47:24 -0600

    Corrected Swift UI view

---
+++ Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Sebastian Pucher on 7/1/24.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
