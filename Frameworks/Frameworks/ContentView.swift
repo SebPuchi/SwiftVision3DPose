1f74d7d5f2a42b3e173c3862bce651b63e7461fd
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2024-07-01 12:00:12 -0600

    setup

---
+++ Frameworks/Frameworks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Frameworks
+//
+//  Created by Sebastian Pucher on 7/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Frameworks/Frameworks/FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworksApp.swift
+//  Frameworks
+//
+//  Created by Sebastian Pucher on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
