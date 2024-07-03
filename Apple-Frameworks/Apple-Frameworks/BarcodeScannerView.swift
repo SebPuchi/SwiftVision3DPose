f9fdc6c8ce7fe523dc74e22d20495d75a6258b5e
SebPuchi <100731345+SebPuchi@users.noreply.github.com>
2024-07-03 10:39:03 -0600

    clean

---
+++ Apple-Frameworks/Apple-Frameworks/BarcodeScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  BarcodeScannerView.swift
+//  Apple-Frameworks
+//
+//  Created by Sebastian Pucher on 7/3/24.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
